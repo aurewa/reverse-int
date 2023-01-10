d63f6d035ec6d4586edd344487ee73ce97760956
Alexander <aurewa@gmail.com>
2023-01-11 00:55:12 +0300

    feat: write reverse function logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    var strN = Math.abs(n).toString();
+    var result = '';
+
+    for(var i = strN.length -1; i >=0 ; i--){
+        result += strN[i];
+    }
+    
+    return Number(result);
 }
